831a4a89eefbc87731907dfce95d8a3572bacf67
Adam Dobrzeniewski <dobrzeniewskia@gmail.com>
2022-01-28 01:10:41 +0100

    jeszcze jest git

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Komponent from "./Komponent";
 
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
 const App = () => {
   return (
     <div>
       Poziomy:
-      <Komonent />
+      {sum(20, 10)}
+      <Komponent />
     </div>
   );
 };
